faaa5e03f6e6b92c07b5ac88a4b19288a789ab5d
scarletgv <scarletgv@hotmail.com>
2019-11-11 09:15:39 -0300

    Commit 5

--- src/Customer.java
+++ src/Customer.java
@@ -14,24 +14,7 @@ class Customer {
    }
 
    public double amountFor(Rental aRental) {
-       double result = 0;
-
-       switch (aRental.getMovie().getPriceCode()) {
-       case Movie.REGULAR:
-            result += 2;
-            if (aRental.getDaysRented() > 2)
-                result += (aRental.getDaysRented() - 2) * 1.5;
-            break;
-       case Movie.NEW_RELEASE:
-            result += aRental.getDaysRented() * 3;
-            break;
-       case Movie.CHILDRENS:
-            result += 1.5;
-            if (aRental.getDaysRented() > 3)
-                result += (aRental.getDaysRented() - 3) * 1.5;
-            break;
-        }
-       return result;
+   	  return aRental.getCharge();
     }
   
    public String statement() {
@@ -42,7 +25,7 @@ class Customer {
       while (rentals.hasMoreElements()) {
          double thisAmount = 0;
          Rental each = (Rental) rentals.nextElement();
-         thisAmount = amountFor(each);
+         thisAmount = each.getCharge();
          frequentRenterPoints ++;
 
          if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
