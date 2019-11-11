f2305a53cee0c8a1f57d0eaebfe242664db5f6d0
scarletgv <scarletgv@hotmail.com>
2019-11-11 09:34:45 -0300

    Commit 10

--- src/Movie.java
+++ src/Movie.java
@@ -1,26 +1,46 @@
 public class Movie {
 
-  public static final int  CHILDRENS = 2;
-  public static final int  REGULAR = 0;
-  public static final int  NEW_RELEASE = 1;
+    public static final int  CHILDRENS = 2;
+    public static final int  REGULAR = 0;
+    public static final int  NEW_RELEASE = 1;
 
-  private String _title;
-  private int _priceCode;
+    private String _title;
+    private Price _price;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
-  }
+    public Movie(String name, int priceCode) {
+        _title = name;
+        setPriceCode(priceCode);
+    }
+    
+    public int getPriceCode() {
+        return _price.getPriceCode();
+    }
+   
+    public void setPriceCode(int arg) {
+        switch (arg) {
+        case REGULAR:
+            _price = new RegularPrice();
+            break;
+        case CHILDRENS:
+            _price = new ChildrensPrice();
+            break;
+        case NEW_RELEASE:
+            _price = new NewReleasePrice();
+            break;
+        default:
+            throw new IllegalArgumentException("Incorrect Price Code");
+        }
+    }
 
-  public int getPriceCode() {
-      return _priceCode;
-  }
+    public String getTitle (){
+        return _title;
+    };
 
-  public void setPriceCode(int arg) {
-     _priceCode = arg;
-  }
+    double getCharge(int daysRented) {
+        return _price.getCharge(daysRented);
+    }
 
-  public String getTitle (){
-      return _title;
-  };
+    int getFrequentRenterPoints(int daysRented) {
+        return _price.getFrequentRenterPoints(daysRented);
+    }
 }
